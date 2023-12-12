8e124d6ddaa3262e2b42739f4dfcbfd106911955
linh <klinh2212112@gmail.com>
2023-12-13 00:38:31 +0700

    fix again

--- upload-img.php
+++ upload-img.php
@@ -17,8 +17,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
   <link rel="stylesheet" href="./css-design/upload.css">
   <link rel="stylesheet" href="./fonts/themify-icons/themify-icons.css">
@@ -49,7 +51,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <!-- search key -->
         <form class="d-flex" role="search">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search" aria-describedby="search-icon">
+            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search"
+              aria-describedby="search-icon">
             <button class="input-group-text" id="search-icon" type="submit">
               <i class="ti-search"></i>
             </button>
@@ -126,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           </div>
 
           <div class="post-img">
-            <input type="submit" value="Post" name="submit" class="btn btn-primary">
+            <input type="submit" value="Post" name="post-img" class="btn btn-primary">
           </div>
         </aside>
       </div>
