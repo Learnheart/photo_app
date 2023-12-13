013f283e15aa106342020927951e386c3f8ad8f0
linh <klinh2212112@gmail.com>
2023-12-13 14:11:27 +0700

    create album in user profile

--- user-profile.php
+++ user-profile.php
@@ -10,6 +10,33 @@ if ($_SESSION["role"] !== "User") {
   header("Location: access-denied.php");
   exit();
 }
+
+// Get the user ID from the session
+$userId = $_SESSION["user"];
+
+// Handle the form submission or UI interaction
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["createAlbum"])) {
+  include "database.php"; // Include your database connection file
+
+  // Sanitize and validate the album name (you can add more validation)
+  $albumName = mysqli_real_escape_string($conn, $_POST["albumName"]);
+
+  // Insert a new album record
+  $sql = "INSERT INTO album (albumName, userId) VALUES (?, ?)";
+  $stmt = mysqli_prepare($conn, $sql);
+
+  // Bind parameters
+  mysqli_stmt_bind_param($stmt, "si", $albumName, $userId);
+
+  // Execute the statement
+  mysqli_stmt_execute($stmt);
+
+  // Close the statement
+  mysqli_stmt_close($stmt);
+
+  // Redirect to a page or show a success message
+  exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,10 +45,8 @@ if ($_SESSION["role"] !== "User") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
   <link rel="stylesheet" href="./css-design/profile.css">
   <link rel="stylesheet" href="./fonts/themify-icons/themify-icons.css">
@@ -52,8 +77,7 @@ if ($_SESSION["role"] !== "User") {
         <!-- search key -->
         <form class="d-flex" role="search">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search"
-              aria-describedby="search-icon">
+            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search" aria-describedby="search-icon">
             <button class="input-group-text" id="search-icon" type="submit">
               <i class="ti-search"></i>
             </button>
@@ -74,7 +98,12 @@ if ($_SESSION["role"] !== "User") {
         </a>
       </div>
     </nav>
-
+    <!-- Adding new album -->
+    <form method="post" action="user-profile.php">
+      <label for="albumName">Album Name:</label>
+      <input type="text" id="albumName" name="albumName" required>
+      <button type="submit" name="createAlbum">Create Album</button>
+    </form>
     <div class="btn">
       <a href="./homepage.php">Back</a>
     </div>
