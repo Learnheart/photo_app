229bdaf0b9a893ca0b624478e6493f0182ea6199
linh <klinh2212112@gmail.com>
2023-12-13 14:29:29 +0700

    display 3 image each row

--- homepage.php
+++ homepage.php
@@ -19,8 +19,10 @@ if ($_SESSION["role"] !== "User") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
   <link rel="stylesheet" href="./css-design/hompage.css">
   <link rel="stylesheet" href="./css-design/hompage_mobile.css">
@@ -53,7 +55,8 @@ if ($_SESSION["role"] !== "User") {
         <!-- search key -->
         <form class="d-flex" role="search">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search" aria-describedby="search-icon">
+            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search"
+              aria-describedby="search-icon">
             <button class="input-group-text" id="search-icon" type="submit">
               <i class="ti-search"></i>
             </button>
@@ -107,20 +110,42 @@ if ($_SESSION["role"] !== "User") {
     <hr class="hr-nav">
     <!-- Page content -->
     <main class="page-content">
-      <?php
-      $sql = "SELECT photoPath FROM photo ORDER BY photoId DESC";
-      $res = mysqli_query($conn,  $sql);
+      <div class="container">
+        <?php
+        include('database.php');
+        $sql = "SELECT photoPath FROM photo ORDER BY photoId DESC";
+        $res = mysqli_query($conn, $sql);
 
-      if (mysqli_num_rows($res) > 0) {
-        while ($images = mysqli_fetch_assoc($res)) {  ?>
-
+        if (mysqli_num_rows($res) > 0) {
+            $counter = 0; // Counter to determine when to start a new row
+            while ($images = mysqli_fetch_assoc($res)) {
+                if ($counter % 3 == 0) {
+                    // Start a new row for every 3 images
+                    echo '<div class="row">';
+                }
+                ?>
+        <div class="col">
           <div class="alb">
-            <img src="uploads/<?= $images['photoPath'] ?>">
+            <img src="uploads/<?= $images['photoPath'] ?>" class="img-fluid" alt="Image">
           </div>
+        </div>
+        <?php
+                if ($counter % 3 == 2) {
+                    // Close the row after every 3 images
+                    echo '</div>';
+                }
+                $counter++;
+            }
 
-      <?php }
-      } ?>
+            // Close the row if there are remaining images
+            if ($counter % 3 != 0) {
+                echo '</div>';
+            }
+        }
+        ?>
+      </div>
     </main>
+
     <!-- Pagination -->
     <!-- Footer -->
     <!-- Logout button -->
--- upload-img.php
+++ upload-img.php
@@ -106,10 +106,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
   </script>
   <link rel="stylesheet" href="./css-design/upload.css">
   <link rel="stylesheet" href="./fonts/themify-icons/themify-icons.css">
@@ -140,8 +138,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <!-- search key -->
         <form class="d-flex" role="search">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search"
-              aria-describedby="search-icon">
+            <input type="text" class="form-control" placeholder="Search keyword" aria-label="Search" aria-describedby="search-icon">
             <button class="input-group-text" id="search-icon" type="submit">
               <i class="ti-search"></i>
             </button>
